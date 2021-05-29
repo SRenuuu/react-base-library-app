86e1c6aa3278d7539a752e2a54577fd96f209656
SandulR <sandulrenuja@gmail.com>
2021-05-30 00:01:10 +0530

    Completed Author CRUD, defined data types separately

--- src/components/LibrarySection/AuthorSection/AddAuthor.tsx
+++ src/components/LibrarySection/AuthorSection/AddAuthor.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import {Col, Row} from "react-bootstrap";
 
+// Typechecking with PropTypes
+type AddAuthorProps = {
+    onAddClick: () => void
+}
+
 // Main component
-const AddAuthor: React.FC = (props) => {
+const AddAuthor: React.FC<AddAuthorProps> = (props) => {
+
+    // Defining props
+    const {onAddClick} = props;
 
     return (
         <Row className='add-author mt-2'>
-            <Col>
+            <Col onClick={onAddClick}>
                 <i className='feather-plus mr-2'/>
                 <label>Add Author</label>
             </Col>
--- src/components/LibrarySection/AuthorSection/AuthorList.tsx
+++ src/components/LibrarySection/AuthorSection/AuthorList.tsx
@@ -2,15 +2,41 @@ import React from "react";
 import {Col, Row} from "react-bootstrap";
 
 // Component imports
+import Author from "./Author";
+
+// Data types imports
+import {IAuthor} from "../../../types/LibraryDataTypes";
+
+// Typechecking with PropTypes
+type AuthorListProps = {
+    authors: IAuthor[]
+    onUpdateClick: (authorIndex: number) => void
+    onRemoveClick: (authorIndex: number) => void
+}
 
 // Main component
-const AuthorList: React.FC = (props) => {
+const AuthorList: React.FC<AuthorListProps> = (props) => {
+
+    // Defining props
+    const {authors, onUpdateClick, onRemoveClick} = props;
+
+    // Custom function for rendering author list
+    const renderAuthors = () => {
+        if (authors.length === 0) {
+            return;
+        }
+        return authors.map((author: IAuthor, index: number) => {
+            return <Author author={author} key={index} num={index + 1} onAuthorUpdateClick={onUpdateClick}
+                           onAuthorRemoveClick={onRemoveClick}/>
+        });
+    }
 
     return (
         <Row className="author-list pt-2">
             <Col xs={12}>
-                <label className='empty-list font-italic'>No authors listed here</label>
+                {authors.length === 0 && <label className='empty-list font-italic'>No authors listed here</label>}
                 <ul className="px-0 ml-0 pt-lg-1 list-unstyled">
+                    {renderAuthors()}
                 </ul>
             </Col>
         </Row>
