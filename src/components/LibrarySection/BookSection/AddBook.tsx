c9a8f7ed66f930b4f5ecb1493658a3c9da70c3bb
SandulR <sandulrenuja@gmail.com>
2021-05-30 00:55:00 +0530

    Completed Book CRUD, implemented react-select for author selection, added toasts and action-confirmations

--- src/components/LibrarySection/BookSection/AddBook.tsx
+++ src/components/LibrarySection/BookSection/AddBook.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import {Col, Row} from "react-bootstrap";
 
+// Typechecking with PropTypes
+type AddBookProps = {
+    onAddClick: () => void
+}
+
 // Main component
-const AddBook: React.FC = (props) => {
+const AddBook: React.FC<AddBookProps> = (props) => {
+
+    // Defining props
+    const {onAddClick} = props;
 
     return (
         <Row className='add-book mt-2'>
-            <Col>
+            <Col onClick={onAddClick}>
                 <i className='feather-plus mr-2'/>
                 <label>Add Book</label>
             </Col>
--- src/components/LibrarySection/BookSection/Book.tsx
+++ src/components/LibrarySection/BookSection/Book.tsx
@@ -1,20 +1,36 @@
 import React from "react";
 import {Col, Row} from "react-bootstrap";
 
+// Data types imports
+import {IBook} from "../../../Types/LibraryDataTypes";
+
 // Action confirmation imports
 import {confirmAlert} from "react-confirm-alert";
 
+// Typechecking with PropTypes
+type BookProps = {
+    book: IBook
+    num: number
+    onBookUpdateClick: (bookIndex: number) => void
+    onBookRemoveClick: (bookIndex: number) => void
+}
+
 // Main component
-const Book: React.FC = (props) => {
+const Book: React.FC<BookProps> = (props) => {
+
+    // Defining props
+    const {book, num, onBookUpdateClick, onBookRemoveClick} = props;
 
     return (
         <li className='py-2'>
             <Row>
                 <Col xs={8} md={9}>
-                    <label className="my-1"/>
+                    <label className="my-1">{num}. {book.title}</label>
                 </Col>
                 <Col xs={4} md={3} className='icons text-right pr-0 pr-sm-2 pr-md-3'>
-                    <i className='feather-edit text-warning pr-4 pr-lg-3'/>
+                    <i className='feather-edit text-warning pr-4 pr-lg-3' onClick={
+                        () => onBookUpdateClick(num - 1)
+                    }/>
                     <i className='feather-trash-2 text-danger pr-3 pr-lg-3' onClick={
                         () => confirmAlert({
                             title: 'Confirm action',
@@ -22,8 +38,7 @@ const Book: React.FC = (props) => {
                             buttons: [
                                 {
                                     label: 'Yes',
-                                    onClick: () => {
-                                    },
+                                    onClick: () => onBookRemoveClick(num - 1),
                                 },
                                 {
                                     label: 'No',
