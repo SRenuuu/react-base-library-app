b74bbe18808cfe193f37aa03bcca4304fb241bf4
SandulR <sandulrenuja@gmail.com>
2021-05-29 22:02:24 +0530

    Completed Author UX with toasts and action-confirmation

--- src/components/LibrarySection/AuthorSection/AuthorHeader.tsx
+++ src/components/LibrarySection/AuthorSection/AuthorHeader.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 
+// Main component
 const AuthorHeader: React.FC = () => {
     return (
-        <h2 className='author-header mt-2 mb-4 pb-3'>Authors</h2>
+        <h2 className='author-header mt-1 mt-lg-0 mb-3 mb-md-4 pb-2 pb-md-2 pb-lg-3'>Authors</h2>
     )
 }
+
 export default AuthorHeader;
