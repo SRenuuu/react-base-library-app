7e6541eff9fdb932685e5bb7585dfc3dbb31749f
SandulR <sandulrenuja@gmail.com>
2021-05-30 00:55:00 +0530

    Completed Book CRUD, implemented react-select for author selection, added toasts and action-confirmations

--- src/types/LibraryDataTypes.ts
+++ src/types/LibraryDataTypes.ts
@@ -3,3 +3,14 @@
 export interface IAuthor {
     name: string
 }
+
+export interface IBook {
+    title: string
+    price: string
+    author: IAuthor | null
+}
+
+export type SelectOption = {
+    value: string
+    label: string
+}
