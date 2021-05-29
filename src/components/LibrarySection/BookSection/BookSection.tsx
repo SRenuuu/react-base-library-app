590e295a0d9408b94164a4106fa1fa66ec36266e
SandulR <sandulrenuja@gmail.com>
2021-05-29 22:37:37 +0530

    Removed Book section files

--- src/components/LibrarySection/BookSection/BookSection.tsx
+++ src/components/LibrarySection/BookSection/BookSection.tsx
@@ -1,36 +1,17 @@
-import React, {useState} from "react";
+import React from 'react';
 import {Col, Row} from "react-bootstrap";
 
-// Component imports
-import BookList from "./BookList";
-import AddBook from "./AddBook";
-import BookForm from "./BookForm";
-import BookHeader from "./BookHeader";
-
-// Toast notifications imports
-
-// Main component
-const BookSection: React.FC = (props) => {
-
-    // Usa state and initialize
-    const [isFormVisible, setIsFormVisible] = useState(false);
-
-    // Handler functions
-    const handleOnFormClose = () => {
-        setIsFormVisible(false);
-    }
-
+const BookSection = () => {
     return (
-        <Row className="book-section px-0 px-md-2 px-lg-3">
-            <Col>
-                <BookHeader/>
-                <BookList/>
-                <AddBook/>
-                {isFormVisible &&
-                <BookForm/>}
-            </Col>
-        </Row>
-    )
-}
+        <>
+            <Row>
+                <Col>
+                    Book Section
+                </Col>
+            </Row>
+
+        </>
+    );
+};
 
 export default BookSection;
