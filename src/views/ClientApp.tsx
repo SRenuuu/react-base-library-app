c26fb24a53836d052874282188d565f5982224d7
SandulR <sandulrenuja@gmail.com>
2021-06-03 11:51:11 +0530

    Added a navbar and react-router

--- src/views/ClientApp.tsx
+++ src/views/ClientApp.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
-import {Container} from "react-bootstrap";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 // Component imports
-import LibrarySection from "../components/LibrarySection/LibrarySection";
 import FooterSection from "../components/FooterSection/FooterSection";
-import WelcomeSection from "../components/WelcomeSection/WelcomeSection";
+import HeaderSection from "../components/HeaderSection/HeaderSection";
+import HomePage from "../components/Pages/HomePage/HomePage";
+import AboutPage from "../components/Pages/AboutPage/AboutPage";
+import ContactPage from "../components/Pages/ContactPage/ContactPage";
 
 // Main component
 const ClientApp: React.FC = () => {
     return (
         <>
-            <Container fluid>
-                <WelcomeSection/>
-                <LibrarySection/>
+            <Router>
+                <HeaderSection/>
+                <Switch>
+                    <Route exact path="/home" component={HomePage}/>
+                    <Route path="/about" component={AboutPage}/>
+                    <Route path="/contact" component={ContactPage}/>
+                    <Route path="/" component={HomePage}/>
+                </Switch>
                 <FooterSection/>
-            </Container>
+            </Router>
         </>
     );
 };
